2a53e80927d9e93acbf1244fcc2f7edffccb1f3f
Midgar <80575182+JesseCaddell@users.noreply.github.com>
2024-02-21 20:02:09 -0800

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const AlertButton = ({ message, children }) => {
+  const handleClick = () => {
+    alert(message);
+  };
+
+  return <button onClick={handleClick}>{children}</button>;
+};
+
+const Toolbar = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AlertButton message="First message">Click 1</AlertButton>
+      <AlertButton message="Second message">Click 2</AlertButton>
     </div>
   );
-}
+};
 
-export default App;
+export default Toolbar;
